16deeed27d61c164c740b550f50dfc3549e4dc1e
Robin-Hood-zjw <jiawenz@umich.edu>
2022-08-22 21:24:20 -0400

    Task001-Layout/V.08.22.2022-09.24.PM/Deploy libraties

--- events-app/src/redux/store.js
+++ events-app/src/redux/store.js
@@ -4,8 +4,7 @@ import comboReducers from "./comboReducers";
 
 const store = createStore(
   comboReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ /
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
